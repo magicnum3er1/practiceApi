33247a616f83ee7e9df1e0cc109c26e8319a1571
majorstirrer <magickotlovski@gmail.com>
2020-04-29 11:09:32 +0100

    jeszcze dziala

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ var app = express();
 
 var request = require('request');
 request('https://jsonplaceholder.typicode.com/users/1',function(error, response, body){
-if(error){
-    console.log("smth wrong")
-    console.log("error")
-}else {
-    if(response.statusCode == 200){
-        console.log(body) 
-    }
+if(!error && response.statusCode == 200){
+    console.log(body)
+   
 }
 })
 
