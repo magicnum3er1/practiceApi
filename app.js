0a6cd8816d1f7f3a0fc36673a56681b10c33787c
majorstirrer <magickotlovski@gmail.com>
2020-04-30 11:11:57 +0100

    adding JSON.parse to app.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var app = express();
 
 var request = require('request');
 request('https://jsonplaceholder.typicode.com/users/1',function(error, response, body){
-if(!error && response.statusCode == 200){
-    console.log(body)
+  if(!error && response.statusCode == 200){
+      var parsedData = JSON.parse(body);
+    console.log(parsedData)
    
 }
 })
